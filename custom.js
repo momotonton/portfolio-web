c8c8b9518c8a79a20d0756b912f5873747fb840a
momotonton <justartwo@daum.net>
2021-03-01 22:31:32 +0900

    mobile trigger

--- custom.js
+++ custom.js
@@ -4,9 +4,13 @@ $(function(){
     $(this).toggleClass('active')
     $('.gnb').toggleClass('active')
   })
-  $('section').click(function(){
+  $('section, .menu a').click(function(){
     $('.gnb').removeClass('active')
     $('.trigger').removeClass('active')
-
 })
+// Smooth Scrolling
+  $('.menu a').click(function(e){
+    $.scrollTo(this.hash || 0, 700)
+
+  })
 })
